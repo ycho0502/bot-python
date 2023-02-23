7bd24b4274f6a1f8bd29de7727fedfd89a7e1e8d
Yunho <ycho0502@gmail.com>
2023-02-23 14:35:23 -0800

    merging

--- testbsdata.py
+++ testbsdata.py
@@ -6,6 +6,8 @@ links = ['whitecastletours.com', 'gowindstar.com', 'arrowstagelines.com', 'sunsh
 
 # Need header to mimic a real browser and avoid being detected as a scraper
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+# response = requests.get(url,headers=headers)
+# soup = BeautifulSoup(response.content, 'html.parser')
 
 for link in links:
     response = requests.get('http://' + link,headers=headers)
@@ -20,11 +22,9 @@ for link in links:
     else:
         print(f"{link}: h1 tag not found")
 
-# response = requests.get(url,headers=headers)
-# soup = BeautifulSoup(response.content, 'html.parser')
 
 # outputs initial tag with given id value
-result_by_id = soup.find(id="1245374334")
+# result_by_id = soup.find(id="1245374334")
 
 # outputs initial tag with input and id value
 # result_by_input_id = soup.find('input', {'id': '1245374334'})
